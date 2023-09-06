d7578a8ec6665f5224418145cb5883aa31740f0f
Hunter Andersen <andersen.hunter42@gmail.com>
2023-09-06 07:32:00 -0700

    Added operators exercise

---
+++ operators/practice.js
@@ -0,0 +1,11 @@
+//Write code that determines and prints the value of c
+//Use the pythagorean theorem (A squared plus B squared equals C squared) to help you achieve the result
+//Given the following...
+const a = 3;
+const b = 4;
+//What is c?
+
+
+//Sally is competing in an ultra marathon. The race lasts 4 days at maximum, after which the attendants working the race
+//will force contestants to stop running and make sure they're okay. Sally has been running for 88 hours.
+//How many seconds does she have left to reach the finish line before the race is stopped?
